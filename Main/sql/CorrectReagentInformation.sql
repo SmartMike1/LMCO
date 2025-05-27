70538da80a295983410101904590c2f796d2a800
SmartMike1 <mikepaderin456570@yandex.ru>
2025-05-27 14:17:40 +0300

    Delete Main directory

--- Main/sql/CorrectReagentInformation.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR ALTER PROCEDURE CorrectReagentInformation
-	-- Add the parameters for the stored procedure here
-	@Param1 int
-AS
-BEGIN
-	SELECT * FROM Реагенты 
-	WHERE КодРеагента = @Param1
-END
