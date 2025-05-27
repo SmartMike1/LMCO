cdd950b37156294bef6a966b3690792dd02868f4
SmartMike1 <mikepaderin456570@yandex.ru>
2025-05-27 14:18:23 +0300

    Add files via upload

---
+++ sql/AllMeasures.sql
@@ -0,0 +1,12 @@
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE OR ALTER PROCEDURE AllMeasures 
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	SELECT  ороткоеЌазвание≈д»зм
+	FROM —ловарь≈диниц»змерени€
+END
